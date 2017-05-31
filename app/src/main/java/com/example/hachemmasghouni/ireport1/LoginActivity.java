58dae1730bc2407c38aee77dae9624fff89864db
AviKenz <kennefoue@yahoo.fr>
2017-05-31 17:59:43 +0200

    *New Data base implementation (000webhost mit php) .the register method work fine .login work fine -> modified the php to check if user exist before creating a new one

--- app/src/main/java/com/example/hachemmasghouni/ireport1/LoginActivity.java
+++ app/src/main/java/com/example/hachemmasghouni/ireport1/LoginActivity.java
@@ -2,17 +2,19 @@ package com.example.hachemmasghouni.ireport1;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v7.app.ActionBar;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
-import android.widget.Toast;
-import android.database.Cursor;
-import android.database.sqlite.SQLiteDatabase;
-import android.database.sqlite.SQLiteOpenHelper;
+
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 
 public class LoginActivity extends AppCompatActivity {
@@ -22,16 +24,58 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        EditText email = (EditText) findViewById(R.id.etEmail);
-        EditText password = (EditText) findViewById(R.id.etPassword);
-        Button login = (Button) findViewById(R.id.bLogin);
-        TextView registerLink = (TextView) findViewById(R.id.tRegister);
+        final EditText etEmail = (EditText) findViewById(R.id.etEmail);
+        final EditText etPassword = (EditText) findViewById(R.id.etPassword);
+        Button bLogin = (Button) findViewById(R.id.bLogin);
+        TextView tRegisterLink = (TextView) findViewById(R.id.tRegister);
 
-        registerLink.setOnClickListener(new View.OnClickListener() {
+        tRegisterLink.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent registerIntent = new Intent(LoginActivity.this, SignupActivity.class);
                 LoginActivity.this.startActivity(registerIntent);
+            }
+        });
+
+        bLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final String email = etEmail.getText().toString();
+                final String password = etPassword.getText().toString();
+
+                Response.Listener<String> responseListener = new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        try {
+                            JSONObject jsonResponse = new JSONObject(response);
+                            boolean success = jsonResponse.getBoolean("success");
+                            if(success) {
+                                String fullName = jsonResponse.getString("fullName");
+                                String email = jsonResponse.getString("email");
+                                String mobile = jsonResponse.getString("mobile");
+
+                                Intent dashboardIntent = new Intent(LoginActivity.this, Dashboard.class);
+                                dashboardIntent.putExtra("fullName", fullName);
+                                dashboardIntent.putExtra("email", email);
+                                dashboardIntent.putExtra("mobile", mobile);
+
+                                LoginActivity.this.startActivity(dashboardIntent);
+                            } else {
+                                AlertDialog.Builder alertBuilder = new AlertDialog.Builder(LoginActivity.this);
+                                alertBuilder.setMessage("Login Failed")
+                                        .setNegativeButton("Retry", null)
+                                        .create()
+                                        .show();
+                            }
+                        } catch(JSONException e) {
+                            e.printStackTrace();
+                        }
+
+                    }
+                };
+                LoginRequest loginRequest = new LoginRequest(email, password, responseListener);
+                RequestQueue queue = Volley.newRequestQueue(LoginActivity.this);
+                queue.add(loginRequest);
             }
         });
 
