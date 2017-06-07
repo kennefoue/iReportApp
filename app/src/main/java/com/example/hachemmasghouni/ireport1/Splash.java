efc20b9907f9e17e91154aefe85ebf175be2b49b
hachemmas <masghouni.hachem@gmail.com>
2017-06-07 14:30:41 +0200

    SplashActivity implemented (welcome page)

---
+++ app/src/main/java/com/example/hachemmasghouni/ireport1/Splash.java
@@ -0,0 +1,58 @@
+package com.example.hachemmasghouni.ireport1;
+
+import android.content.Intent;
+import android.view.WindowManager;
+
+import com.daimajia.androidanimations.library.Techniques;
+import com.viksaa.sssplash.lib.activity.AwesomeSplash;
+import com.viksaa.sssplash.lib.cnst.Flags;
+import com.viksaa.sssplash.lib.model.ConfigSplash;
+
+
+
+public class Splash extends AwesomeSplash {
+
+//No need for the layout thats why uncommented
+  /*  @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }*/
+
+    @Override
+    public void initSplash(ConfigSplash configSplash) {
+
+        android.support.v7.app.ActionBar actionBar = getSupportActionBar();
+        actionBar.hide();
+
+
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        //Background animation and colours
+        configSplash.setBackgroundColor(R.color.colorPrimaryLight);
+        configSplash.setAnimCircularRevealDuration(3000);
+        configSplash.setRevealFlagX(Flags.REVEAL_LEFT);
+        configSplash.setRevealFlagX(Flags.REVEAL_BOTTOM);
+
+
+        //Logo and duration
+        configSplash.setLogoSplash(R.drawable.ic_eye);
+        configSplash.setAnimLogoSplashDuration(3000);
+        configSplash.setAnimLogoSplashTechnique(Techniques.Landing);
+
+
+        //Title on the screen
+        configSplash.setTitleSplash(getString(R.string.text_report));
+        configSplash.setTitleTextColor(R.color.icons);
+        configSplash.setTitleTextSize(24f);
+        configSplash.setAnimPathFillingDuration(2000);
+        configSplash.setAnimTitleTechnique(Techniques.BounceIn);
+    }
+
+
+    @Override
+    public void animationsFinished() {
+        startActivity(new Intent(Splash.this,MainActivity.class));
+    }
+}
+
