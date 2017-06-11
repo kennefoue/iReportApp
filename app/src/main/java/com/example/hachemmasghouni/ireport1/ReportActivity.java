ab6952902c671081fac3adcc662d657bf612d41c
kennefoue <kennefoue@yahoo.fr>
2017-06-11 10:19:43 +0200

    *Report Layout Finished !*

--- app/src/main/java/com/example/hachemmasghouni/ireport1/ReportActivity.java
+++ app/src/main/java/com/example/hachemmasghouni/ireport1/ReportActivity.java
@@ -5,7 +5,9 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
+import com.melnykov.fab.FloatingActionButton;
 import com.google.android.gms.location.places.Place;
 import com.google.android.gms.location.places.ui.PlacePicker;
 
@@ -26,8 +28,8 @@ public class ReportActivity extends AppCompatActivity{
 
     private final int REQUEST_CODE_PLACEPICKER = 1;
 
-    private Button getLocationBtn;
-    private TextView getLocationTv;
+    private ImageView getLocationIv;
+    private TextView pickerResult;
 
 
 
@@ -38,9 +40,10 @@ public class ReportActivity extends AppCompatActivity{
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_report);
 
-        getLocationBtn = (Button) findViewById(R.id.btn_get_location);
-        getLocationTv = (TextView) findViewById(R.id.tv_get_location);
-        getLocationBtn.setOnClickListener(new View.OnClickListener() {
+        getLocationIv = (ImageView) findViewById(R.id.iv_get_location);
+        pickerResult = (TextView) findViewById(R.id.result_text_location);
+
+        getLocationIv.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 startPlacePickerActivity();
@@ -69,7 +72,8 @@ public class ReportActivity extends AppCompatActivity{
 
     private void displaySelectedPlaceFromPlacePicker(Intent data){
         Place placeSelected = PlacePicker.getPlace(data, this);
-        getLocationTv.setText(placeSelected.getAddress().toString());
+        pickerResult.setText(placeSelected.getAddress().toString());
+        pickerResult.setVisibility(View.VISIBLE);
     }
 
 
